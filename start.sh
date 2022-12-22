1ccc2eaf4420bda9ec8bdf1f3220e4058620ec1a
Etchial <98379633+Etchial@users.noreply.github.com>
2022-12-22 22:31:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/GreyMatterbots/url-auto-delete-shortener-bot /Eva
+  git clone https://github.com/Etchial/Movie-File-Bot /Movie-File-Bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Eva
+  git clone $UPSTREAM_REPO /Movie-File-Bot
 fi
-cd /Eva
+cd /Movie-File-Bot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
